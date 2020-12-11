b3fa57064db8e3e282f802219992af7e6db73c96
Matthew Casperson <matthewcasperson@gmail.com>
2020-12-11 10:48:51 +1000

    Used provisioned concurrency

--- src/main/java/works/weave/socks/shipping/StreamLambdaHandler.java
+++ src/main/java/works/weave/socks/shipping/StreamLambdaHandler.java
@@ -4,12 +4,14 @@ import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.Instant;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
